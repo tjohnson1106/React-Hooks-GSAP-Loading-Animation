3afe303b4bc898da4ed54dcd941c8ec880a889a0
Thomas Johnson <tjohnson1106@gmail.com>
2019-10-03 18:53:25 -0400

    created basic svg graphic

---
+++ src/Loader.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Loader = () => {
+  return (
+    <svg viewBox="0 0 150 33.2" width="180" height="150">
+      <circle cx="16.1" cy="16.6" r="16.1" fill="#527abd" />
+      <circle cx="55.2" cy="16.6" r="16.1" fill="#de4431" />
+      <circle cx="94.3" cy="16.6" r="16.1" fill="#f4b61a" />
+      <circle cx="133.4" cy="16.6" r="16.1" fill="#009e52" />
+    </svg>
+  );
+};
+
+export default Loader;
